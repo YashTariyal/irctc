e535a912577b8ef2d9c139cec10fd4d3d9c76266
YashTariyal <yashtariyal4@gmail.com>
2025-09-02 00:17:59 +0530

    added some configs

--- src/main/java/com/irctc_backend/irctc/IrctcApplication.java
+++ src/main/java/com/irctc_backend/irctc/IrctcApplication.java
@@ -2,10 +2,18 @@ package com.irctc_backend.irctc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
+@EnableCaching
+@EnableAsync
+@EnableScheduling
+@EnableTransactionManagement
 public class IrctcApplication {
 
 	public static void main(String[] args) {
