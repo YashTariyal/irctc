2af36ae432acfce7c78e0b2833099efcac1157c2
YashTariyal <yashtariyal4@gmail.com>
2025-09-07 15:23:29 +0530

    added some commit

--- src/main/java/com/irctc_backend/irctc/IrctcApplication.java
+++ src/main/java/com/irctc_backend/irctc/IrctcApplication.java
@@ -3,6 +3,7 @@ package com.irctc_backend.irctc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -12,6 +13,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableAspectJAutoProxy
 @EnableCaching
 @EnableAsync
+@EnableDiscoveryClient
 @EnableScheduling
 @EnableTransactionManagement
 public class IrctcApplication {
